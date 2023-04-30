f2ea16fa21339c48f29f887beb1e2770bc9af382
Anand Krishna <132173284+anandkrishnagit@users.noreply.github.com>
2023-04-30 05:48:40 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        System.out.println("the mutliple no");
+        System.out.println(a * b);
+    }
+}
